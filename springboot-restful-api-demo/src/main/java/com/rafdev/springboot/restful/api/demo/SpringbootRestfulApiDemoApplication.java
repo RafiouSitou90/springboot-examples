a465b1a52ea9926a2b3151b70fb6a8ff7410e5dc
Rafiou Sitou <rafiousitou90@gmail.com>
2021-08-24 12:58:00 -0300

    feat(restful-api-demo): Add OpenAPI docs

--- springboot-restful-api-demo/src/main/java/com/rafdev/springboot/restful/api/demo/SpringbootRestfulApiDemoApplication.java
+++ springboot-restful-api-demo/src/main/java/com/rafdev/springboot/restful/api/demo/SpringbootRestfulApiDemoApplication.java
@@ -1,9 +1,18 @@
 package com.rafdev.springboot.restful.api.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "SpringBoot RESTful API demo",
+                description = "SpringBoot RESTful API demo based on blog",
+                version = "1.0"
+        )
+)
 public class SpringbootRestfulApiDemoApplication {
 
     public static void main(String[] args) {
--- springboot-restful-api-demo/src/main/java/com/rafdev/springboot/restful/api/demo/controllers/CategoryController.java
+++ springboot-restful-api-demo/src/main/java/com/rafdev/springboot/restful/api/demo/controllers/CategoryController.java
@@ -2,6 +2,13 @@ package com.rafdev.springboot.restful.api.demo.controllers;
 
 import com.rafdev.springboot.restful.api.demo.entities.Category;
 import com.rafdev.springboot.restful.api.demo.services.CategoryService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +27,21 @@ public class CategoryController
         this.categoryService = categoryService;
     }
 
+    @Operation(summary = "Create a new category")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Category created successfully!",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = Category.class)
+                                    )
+                            }
+                    )
+            }
+    )
     @PostMapping("")
     public ResponseEntity<Category> saveCategory(@Valid @RequestBody Category category) {
         return new ResponseEntity<>(categoryService.saveCategory(category), HttpStatus.CREATED);
