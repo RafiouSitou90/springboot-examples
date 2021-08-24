f1e5d2981932446946573c3053b76a297e0cbb6e
Rafiou Sitou <rafiousitou90@gmail.com>
2021-08-24 01:02:14 -0300

    RESTful API Demo Initial Commit.

---
+++ springboot-restful-api-demo/src/main/java/com/rafdev/springboot/restful/api/demo/SpringbootRestfulApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rafdev.springboot.restful.api.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfulApiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestfulApiDemoApplication.class, args);
+    }
+
+}
